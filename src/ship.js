f06a3c40b044cb3adab6225bbd6db6f8dcbfca54
Simba Ndewere <ndeweresimba@gmail.com>
2025-02-18 02:21:18 +0000

    remove arrow functions in ship.js factory function and add normal functions

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,17 @@
-function shipObject() {
-  return {};
+function shipObject(shipLength) {
+    return {
+        length: shipLength,
+        sunk: false,
+        hitSum: 0,
+
+        hit: function () {
+            this.hitSum = this.hitSum + 1
+        },
+
+        isSunk: function () {
+            this.sunk = true
+        }
+    }
 }
 
 export default shipObject;
