2c977c636f94556b9a7973c8667d4334eec1cdd4
Simba Ndewere <ndeweresimba@gmail.com>
2025-02-20 13:01:34 +0000

    ship is sunk test

--- src/ship.test.js
+++ src/ship.test.js
@@ -9,3 +9,10 @@ test('ship object', () => {
     expect(shipObject(3)).toHaveProperty("hit");
     expect(shipObject(3)).toHaveProperty("isSunk");
 });
+
+test('ship is sunk test', () => {
+    const data = shipObject(3);
+    expect(data.sunk).not.toBeTruthy();
+    data.isSunk();
+    expect(data.sunk).toBeTruthy();
+});
