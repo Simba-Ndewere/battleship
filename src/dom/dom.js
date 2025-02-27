b4d051544d51ac5c235ac6af945fdca3b5c1afb0
Simba Ndewere <ndeweresimba@gmail.com>
2025-02-27 23:59:50 +0000

    create cells for computer gameboard

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -1,14 +1,20 @@
 class Dom {
 
-    static createBoard1Grid(){
+    static createBoardGrids(){
         const board1 = document.querySelector(".board1");
         const board2 = document.querySelector(".board2");
 
         for(let a = 0; a < 100; a++){
             const cell = document.createElement("div");
+            const cell2 = document.createElement("div");
+
             cell.classList.add("cell");
+            cell2.classList.add("cell2");
+
             cell.id = 'cell' + a;
+            cell2.id = 'cell2' + a;
             board1.appendChild(cell);
+            board2.appendChild(cell2);
         }
     }
 }
