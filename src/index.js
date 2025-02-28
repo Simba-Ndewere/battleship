16ab932ecbd959130d536435af3c83c8744a15d0
Simba Ndewere <ndeweresimba@gmail.com>
2025-02-28 04:09:51 +0000

    create ships from factory object

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import "./index.css";
 import Dom from "./dom/dom.js";
+import ship from './domain/ship.js';
+import gameboard from "./domain/gameboard.js";
 
 const shuffleShips = document.getElementById('shuffle');
 const newGame = document.getElementById('newGame');
 
 Dom.createBoardGrids();
+
+const createShips = () => {
+    const playerGameBoard = gameboard();
+
+    const cruiser = ship(2);
+    const battleship = ship(4);
+    const submarine = ship(3);
+    const aircraft = ship(5);
+    const destroyer = ship(3);
+
+    //console.log(cruiser);
+}
 
 shuffleShips.addEventListener('click', function () {
     console.log("shuffle");
@@ -14,4 +28,5 @@ newGame.addEventListener('click', function() {
     console.log("new game");
 });
 
+window.onload = createShips();
 
