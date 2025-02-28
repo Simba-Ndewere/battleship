989ec7688cc78d50d4cfa498b5b0b2843117408c
Simba Ndewere <ndeweresimba@gmail.com>
2025-02-28 05:02:27 +0000

    test shuffle coordinates

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,7 +1,8 @@
 import gameboard from '../domain/gameboard';
-const gameboardObject = gameboard();
+
 
 test('', () => {
+    const gameboardObject = gameboard();
     expect(gameboardObject.allShipsSunk).toBe(false);
     expect(gameboardObject.shipCoordinates).toEqual([]);
     expect(gameboardObject).toHaveProperty("receiveAttack");
@@ -10,8 +11,30 @@ test('', () => {
 });
 
 test('test adding coordinates', () => {
-    const array = ['C8','F8'];
+    const gameboardObject = gameboard();
+    const array = ['C8', 'F8'];
     gameboardObject.addCoordinates(array);
-    expect(gameboardObject.shipCoordinates).toStrictEqual([['C8','F8']]);
+    expect(gameboardObject.shipCoordinates).toStrictEqual([['C8', 'F8']]);
 });
 
+test('test shuffling ship coordinates', () => {
+    const gameboardObject = gameboard();
+    const array = ['C2', 'F2'];
+    const array2 = ['A3', 'A5'];
+    const array3 = ['B9', 'F9'];
+    const array4 = ['J9', 'J7'];
+    const array5 = ['G5', 'G4'];
+
+    gameboardObject.addCoordinates(array);
+    gameboardObject.addCoordinates(array2);
+    gameboardObject.addCoordinates(array3);
+    gameboardObject.addCoordinates(array4);
+    gameboardObject.addCoordinates(array5);
+
+    gameboardObject.shuffle();
+
+    const coordinates = gameboardObject.shipCoordinates;
+
+    expect(coordinates).not.toEqual(expect.arrayContaining([['C2', 'F2']]));
+
+});
