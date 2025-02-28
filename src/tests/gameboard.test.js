74d7356c9b3eedd5711752ef724b487ac6b53c0c
Simba Ndewere <ndeweresimba@gmail.com>
2025-02-28 04:08:40 +0000

    test adding coordinates to a gameboard

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -5,4 +5,10 @@ test('', () => {
     expect(gameboardObject.allShipsSunk).toBe(false);
     expect(gameboardObject.shipCoordinates).toEqual([]);
     expect(gameboardObject).toHaveProperty("receiveAttack");
+
+    expect(gameboardObject).toHaveProperty("shuffle");
+    const array = ['C8','F8'];
+    gameboardObject.addCoordinates(array);
+    expect(gameboardObject.shipCoordinates).toStrictEqual([['C8','F8']]);
 });
+
