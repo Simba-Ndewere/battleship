af78d9e33a341276697ffb5264334fc4d5dbbcff
Simba Ndewere <ndeweresimba@gmail.com>
2025-02-28 04:15:41 +0000

    extract testing adding coordinates to be in its own test

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,12 +1,15 @@
 import gameboard from '../domain/gameboard';
+const gameboardObject = gameboard();
 
 test('', () => {
-    const gameboardObject = gameboard();
     expect(gameboardObject.allShipsSunk).toBe(false);
     expect(gameboardObject.shipCoordinates).toEqual([]);
     expect(gameboardObject).toHaveProperty("receiveAttack");
 
     expect(gameboardObject).toHaveProperty("shuffle");
+});
+
+test('test adding coordinates', () => {
     const array = ['C8','F8'];
     gameboardObject.addCoordinates(array);
     expect(gameboardObject.shipCoordinates).toStrictEqual([['C8','F8']]);
