44c2916f0987b95140ca24605ee81969372605d1
Simba Ndewere <ndeweresimba@gmail.com>
2025-02-28 01:35:03 +0000

    add shuffle ships and new game event listeners

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import "./index.css";
 import Dom from "./dom/dom.js";
 
+const shuffleShips = document.getElementById('shuffle');
+const newGame = document.getElementById('newGame');
+
 Dom.createBoardGrids();
 
-const randomiseShips = () => {
-    //change ships
-}
+shuffleShips.addEventListener('click', function () {
+    console.log("shuffle");
+});
+
+newGame.addEventListener('click', function() {
+    console.log("new game");
+});
+
+
