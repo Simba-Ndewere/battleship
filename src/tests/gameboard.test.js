e0748a986272b3619e650511f60ed1f2a4e54d40
Simba Ndewere <ndeweresimba@gmail.com>
2025-03-03 11:05:42 +0000

    remove previous tests for rewrite

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -18,23 +18,5 @@ test('test adding coordinates', () => {
 });
 
 test('test shuffling ship coordinates', () => {
-    const gameboardObject = gameboard();
-    const array = ['C2', 'F2'];
-    const array2 = ['A3', 'A5'];
-    const array3 = ['B9', 'F9'];
-    const array4 = ['J9', 'J7'];
-    const array5 = ['G5', 'G4'];
-
-    gameboardObject.addCoordinates(array);
-    gameboardObject.addCoordinates(array2);
-    gameboardObject.addCoordinates(array3);
-    gameboardObject.addCoordinates(array4);
-    gameboardObject.addCoordinates(array5);
-
-    gameboardObject.shuffle();
-
-    const coordinates = gameboardObject.shipCoordinates;
-
-    expect(coordinates).not.toEqual(expect.arrayContaining([['C2', 'F2']]));
-
+  
 });
