3a3cb953ac655b0ff6ec9bb460fa31f2f556606d
Simba Ndewere <ndeweresimba@gmail.com>
2025-03-05 17:01:17 +0000

    clear board coordinates when shuffle selected

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -1,11 +1,11 @@
 
 class Dom {
 
-    static createBoardGrids(){
+    static createBoardGrids() {
         const board1 = document.querySelector(".board1");
         const board2 = document.querySelector(".board2");
 
-        for(let a = 0; a < 100; a++){
+        for (let a = 0; a < 100; a++) {
             const cell = document.createElement("div");
             const cell2 = document.createElement("div");
 
@@ -19,19 +19,31 @@ class Dom {
         }
     }
 
-    static playerDefaultPlacement(ship, direction, startingPosition) {
-        for(let a = 0; a < ship.length; a++){
+    static playerDefaultPlacement(ship, direction, startingPosition, gameboard) {
+        let coordinates = [];
+        for (let a = 0; a < ship.length; a++) {
             const cell = document.getElementById("cell" + startingPosition);
             cell.classList.add("shipPlacement");
 
-            if(direction === 'a'){
+            coordinates.push('cell' + startingPosition);
+            if (direction === 'a') {
                 startingPosition = startingPosition + 1;
             }
 
-            if(direction === 'u'){
+            if (direction === 'u') {
                 startingPosition = startingPosition - 10;
             }
+        }
 
+        gameboard.addCoordinates(coordinates);
+    }
+
+    static clearBoardCells() {
+        for (let a = 0; a < 100; a++) {
+            let cell = document.getElementById("cell" + a);
+            if (cell.classList.contains("shipPlacement")) 
+                cell.classList.remove("shipPlacement");
+            
         }
     }
 }
