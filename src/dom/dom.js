a3212b12a747c17bb0911d1e006026e906cdae9f
Simba Ndewere <ndeweresimba@gmail.com>
2025-03-11 13:10:38 +0000

    add different colours when placing ships

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -1,6 +1,6 @@
 
 class Dom {
-
+    static colours = ["black", "green", "red", "purple", "yellow"];
     static createBoardGrids() {
         const board1 = document.querySelector(".board1");
         const board2 = document.querySelector(".board2");
@@ -19,12 +19,12 @@ class Dom {
         }
     }
 
-    static playerDefaultPlacement(ship, direction, startingPosition, gameboard) {
+    static playerDefaultPlacement(ship, direction, startingPosition, gameboard, colour) {
         let coordinates = [];
         for (let a = 0; a < ship.length; a++) {
             const cell = document.getElementById("cell" + startingPosition);
             cell.classList.add("shipPlacement");
-
+            cell.style.border = "solid 5px " + colour;
             coordinates.push('cell' + startingPosition);
             if (direction === 'a') {
                 startingPosition = startingPosition + 1;
@@ -41,11 +41,26 @@ class Dom {
     static clearBoardCells() {
         for (let a = 0; a < 100; a++) {
             let cell = document.getElementById("cell" + a);
-            if (cell.classList.contains("shipPlacement")) 
+            if (cell.classList.contains("shipPlacement")){
                 cell.classList.remove("shipPlacement");
-            
+                cell.style.border = "none";
+            }
+
         }
     }
+
+    static placeShipsOnBoard(coordinates) {
+        for(let b = 0; b < coordinates.length; b++){
+            for (let a = 0; a < coordinates[b].length; a++) {
+                const cell = document.getElementById("cell" + coordinates[b][a]);
+                cell.classList.add("shipPlacement");
+                cell.style.border = "solid 5px " + this.colours[b];
+            }
+        }
+        
+    }
+
+
 }
 
 export default Dom;
