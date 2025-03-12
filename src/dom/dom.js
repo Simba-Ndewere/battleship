30da79466451416a38a3dbcd42edce798aee62bd
Simba Ndewere <ndeweresimba@gmail.com>
2025-03-12 00:36:17 +0000

    place computer ships on grid board

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -1,6 +1,8 @@
 
 class Dom {
     static colours = ["black", "green", "red", "purple", "yellow"];
+    static colour = 0;
+
     static createBoardGrids() {
         const board1 = document.querySelector(".board1");
         const board2 = document.querySelector(".board2");
@@ -19,13 +21,14 @@ class Dom {
         }
     }
 
-    static playerDefaultPlacement(ship, direction, startingPosition, gameboard, colour) {
+    static playerDefaultPlacement(ship, direction, startingPosition, gameboard) {
         let coordinates = [];
         for (let a = 0; a < ship.length; a++) {
             const cell = document.getElementById("cell" + startingPosition);
             cell.classList.add("shipPlacement");
-            cell.style.border = "solid 5px " + colour;
+            cell.style.border = "solid 5px " + this.colours[this.colour];
             coordinates.push('cell' + startingPosition);
+
             if (direction === 'a') {
                 startingPosition = startingPosition + 1;
             }
@@ -34,7 +37,7 @@ class Dom {
                 startingPosition = startingPosition - 10;
             }
         }
-
+        this.colour = this.colour + 1;
         gameboard.addCoordinates(coordinates);
     }
 
@@ -45,11 +48,10 @@ class Dom {
                 cell.classList.remove("shipPlacement");
                 cell.style.border = "none";
             }
-
         }
     }
 
-    static placeShipsOnBoard(coordinates) {
+    static placePlayerShipsOnBoard(coordinates) {
         for(let b = 0; b < coordinates.length; b++){
             for (let a = 0; a < coordinates[b].length; a++) {
                 const cell = document.getElementById("cell" + coordinates[b][a]);
@@ -57,10 +59,17 @@ class Dom {
                 cell.style.border = "solid 5px " + this.colours[b];
             }
         }
-        
     }
 
-
+    static placeComputerShipsOnBoard(coordinates){
+        for(let b = 0; b < coordinates.length; b++){
+            for (let a = 0; a < coordinates[b].length; a++) {
+                const cell = document.getElementById("cell2" + coordinates[b][a]);
+                cell.classList.add("shipPlacement");
+                cell.style.border = "solid 5px " + this.colours[b];
+            }
+        }
+    }
 }
 
 export default Dom;
