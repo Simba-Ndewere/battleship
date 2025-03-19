0b5eb415fb53e988600112d55457088c79de2fe5
Simba Ndewere <ndeweresimba@gmail.com>
2025-03-19 18:54:54 +0000

    refactor code

--- src/domain/ship.js
+++ src/domain/ship.js
@@ -7,11 +7,11 @@ function shipObject(shipLength, shipIdentification) {
         shipId: shipIdentification,
 
         hit: function () {
-            this.hitSum = this.hitSum + 1
+            this.hitSum = this.hitSum + 1;
         },
 
         isSunk: function () {
-            this.sunk = true
+            return this.sunk;
         },
 
         getCoordinates: function () {
