e9364da628872b7ef00d070d6c92122b4481b73b
Simba Ndewere <ndeweresimba@gmail.com>
2025-03-19 21:59:05 +0000

    check player turn

--- src/index.js
+++ src/index.js
@@ -14,16 +14,17 @@ const computerGameBoard = gameboard();
 
 const currentPlayer = player();
 const computerPlayer = player();
+let playerTurn = 0; // 0 for player and 1 for computer
 
 dom.createBoardGrids();
 
 const createDefaultPlayerShips = () => {
 
-    const cruiser = ship(2, 0);
-    const battleship = ship(4, 1);
-    const submarine = ship(3, 2);
-    const aircraft = ship(5, 3);
-    const destroyer = ship(3, 4);
+    const cruiser = ship(2);
+    const battleship = ship(4);
+    const submarine = ship(3);
+    const aircraft = ship(5);
+    const destroyer = ship(3);
 
     const ships = [cruiser, battleship, submarine, aircraft, destroyer];
 
@@ -56,16 +57,38 @@ newGame.addEventListener('click', (event) => {
 });
 
 playerBoard.addEventListener("click", (event) => {
+    let changeTurn = true;
+
     if (event.target.classList.contains("cell")) {
         let id = Number(event.target.id.substring(10));
-        playerGameBoard.receiveAttack(id, "playerCell");
+        changeTurn = playerGameBoard.receiveAttack(id, "playerCell");
+    }
+
+    if (changeTurn) {
+        dom.hitOrMissDisplay("miss");
+        dom.displayPlayerTurn(0);
+        dom.lockUnlockBoard(1);
+    } else {
+        dom.hitOrMissDisplay("hit");
+        dom.displayPlayerTurn(1);
     }
 });
 
 computerBoard.addEventListener("click", (event) => {
+    let changeTurn = true;
+
     if (event.target.classList.contains("cell2")) {
         let id = Number(event.target.id.substring(12));
-        computerGameBoard.receiveAttack(id, "computerCell");
+        changeTurn = computerGameBoard.receiveAttack(id, "computerCell");
+    }
+
+    if (changeTurn) {
+        dom.lockUnlockBoard(0);
+        dom.hitOrMissDisplay("miss");
+        dom.displayPlayerTurn(1);
+    } else {
+        dom.hitOrMissDisplay("hit");
+        dom.displayPlayerTurn(0);
     }
 });
 
